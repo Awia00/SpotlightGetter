42180780b93611d4d220e6158223bbdb2a028a7a
Anders Wind <awia00@gmail.com>
2016-04-15 12:03:19 +0200

    Added the app project and updated/fixed all dependencies and added gif

---
+++ SpotlightGetterApp/App.xaml.cs
@@ -0,0 +1,44 @@
+using System;
+using Windows.UI.Xaml;
+using System.Threading.Tasks;
+using SpotlightGetterApp.Services.SettingsServices;
+using Windows.ApplicationModel.Activation;
+using Template10.Mvvm;
+using Template10.Common;
+using System;
+using System.Linq;
+
+namespace SpotlightGetterApp
+{
+    sealed partial class App : Template10.Common.BootStrapper
+    {
+        public App()
+        {
+            InitializeComponent();
+            SplashFactory = (e) => new Views.Splash(e);
+
+            #region App settings
+
+            var _settings = SettingsService.Instance;
+            RequestedTheme = _settings.AppTheme;
+            CacheMaxDuration = _settings.CacheMaxDuration;
+            ShowShellBackButton = _settings.UseShellBackButton;
+
+            #endregion
+        }
+
+        public override async Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            await Task.CompletedTask;
+        }
+
+        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            // long-running startup tasks go here
+
+            NavigationService.Navigate(typeof(Views.MainPage));
+            await Task.CompletedTask;
+        }
+    }
+}
+
